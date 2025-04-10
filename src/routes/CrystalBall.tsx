bdd12406b1177f395644e2472e77adbffd9907be
nshiff <nshiff@users.noreply.github.com>
2025-04-10 17:26:36 -0400

    simple implementation for crystal ball a/k/a magic 8-ball

--- src/routes/CrystalBall.tsx
+++ src/routes/CrystalBall.tsx
@@ -1,3 +1,44 @@
+import { useState } from "react";
+
+const RESPONSES = [
+  "It is certain.",
+  "It is decidedly so.",
+  "Without a doubt.",
+  "Yes - definitely.",
+  "You may rely on it.",
+  "As I see it, yes.",
+  "Most likely.",
+  "Outlook good.",
+  "Yes.",
+  "Signs point to yes.",
+  "Reply hazy, try again.",
+  "Ask again later.",
+  "Better not tell you now.",
+  "Cannot predict now.",
+  "Concentrate and ask again.",
+  "Don't count on it.",
+  "My reply is no.",
+  "My sources say no.",
+  "Outlook not so good.",
+  "Very doubtful.",
+];
+
 export function CrystalBall() {
-  return <div>Hello World from CrystalBall</div>;
+  const [prediction, setPrediction] = useState<string | null>(null);
+
+  const handleClick = () => {
+    const updatedPrediction =
+      RESPONSES[Math.floor(Math.random() * RESPONSES.length)];
+    setPrediction(updatedPrediction);
+  };
+
+  return (
+    <div>
+      <em>Think of a yes/no question, then click Predict.</em>
+      <p>
+        <button onClick={handleClick}>Predict</button>
+      </p>
+      <p>{prediction}</p>
+    </div>
+  );
 }
